db036aed81d5c7e0eea0873400408639317641af
florinhegedus <florinxhgds@gmail.com>
2024-09-21 21:32:20 +0300

    chat working

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+import streamlit as st
+from openai import OpenAI
+
+st.title("Get advice on how to improve your conversation:")
+
+# Check if running inside a container
+in_container = os.getenv('IN_CONTAINER', 'False')  # Default to 'False' if not found
+
+# Access the secret from environment variables
+if in_container:
+    api_key = os.getenv("OPENAI_API_KEY")
+else:
+    api_key = st.secrets["OPENAI_API_KEY"]
+client = OpenAI(api_key=api_key)
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-4o-mini"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
